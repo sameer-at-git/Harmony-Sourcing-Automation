614254e9f6edca6ae06e7cc7ef8d313bce6bd565
MD.Sameer Sayed <sameer0sayed@gmail.com>
2025-11-04 12:57:35 +0600

    Navigate Categories

---
+++ modules/navigate_categories.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from config.settings import CATEGORIES_URL
+from modules.admin_login import admin_login
+import time
+
+
+def navigate_categories(driver):
+    admin_login(driver)
+    driver.get(CATEGORIES_URL)
+    time.sleep(2)
+
+    print("Navigated to categories Successfully")
